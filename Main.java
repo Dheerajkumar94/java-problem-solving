f7270060b3e2171e7e30590677714435b4c89567
Dheerajkumar94 <xkumardheeraj@gmail.com>
2024-05-08 22:28:44 +0530

    first print pattern

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    private static Number  number = new Number();
+    public static void main(String[] args) {
+        System.out.println("Hello worlds");
+        number.printPattenr(5);
+    }
+}
